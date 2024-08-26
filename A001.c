423cc156e5cc7ac01ba5cd4e806946820342225b
RaulggSouza <raulggsouza@hotmail.com>
2024-08-26 10:28:46 -0300

    Primeira atividade

---
+++ A001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    int primeiroNumero;
+    int segundoNumero;
+    scanf("%d", &primeiroNumero);
+    scanf("%d", &segundoNumero);
+    printf("%d", (primeiroNumero+segundoNumero));
+    return 0;
+}
