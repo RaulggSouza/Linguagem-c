f7df3f1f1f60bc0998f0429d5d7730ad4e7d2350
RaulggSouza <raulggsouza@hotmail.com>
2024-10-12 12:24:19 -0300

    :sparkles: feat: finish exercise C010

--- Exercicios_de_fixacao_de_C/Lista_C/C010.c
+++ Exercicios_de_fixacao_de_C/Lista_C/C010.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mdc(int a, int b){
+    if (a == 0){
+        return b;
+    }else if (b == 0){
+        return a;
+    }
+    return (mdc(b,(a%b)));
+}
+
 int main(int argc, char const *argv[]){
-    
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d",mdc(a,b));
     return 0;
 }
