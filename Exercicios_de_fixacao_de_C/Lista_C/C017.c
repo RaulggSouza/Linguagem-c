550171ea8ee400f3ed85cced88e59aab1fdae6ea
RaulggSouza <raulggsouza@hotmail.com>
2024-10-14 10:46:40 -0300

    :boom: fix: remove function from exercise C017

--- Exercicios_de_fixacao_de_C/Lista_C/C017.c
+++ Exercicios_de_fixacao_de_C/Lista_C/C017.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/* Recursividade
 int fatorial(int n){
     if (n == 0 || n == 1){
         return 1;
@@ -15,6 +15,24 @@ int main(int argc, char const *argv[]){
     for (int i = n; i >= 0; i--){
         soma_fatorial += fatorial(i);
     }
+    printf("%d", soma_fatorial);
+    return 0;
+}
+*/
+
+int main(int argc, char const *argv[]){
+    int n;
+    scanf("%d", &n);
+    int soma_fatorial = 0;
+    int fatorial;
+    for (int i = n; i > 0; i--){
+        fatorial = i;
+        for (int j = i-1; j > 0; j--){
+            fatorial *= j;
+        }
+        soma_fatorial += fatorial;
+    }
+    soma_fatorial += 1; // fatorial de 0
     printf("%d", soma_fatorial);
     return 0;
 }
