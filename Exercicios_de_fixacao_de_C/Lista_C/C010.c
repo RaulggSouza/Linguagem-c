e3fb1d25f96247618169bd90f8e5bfeaf75911e2
RaulggSouza <raulggsouza@hotmail.com>
2024-10-14 10:30:16 -0300

    :boom: fix: remove function from exercise C010

--- Exercicios_de_fixacao_de_C/Lista_C/C010.c
+++ Exercicios_de_fixacao_de_C/Lista_C/C010.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Recursividade
 int mdc(int a, int b){
     if (a == 0){
         return b;
@@ -9,12 +10,33 @@ int mdc(int a, int b){
     }
     return (mdc(b,(a%b)));
 }
-
 int main(int argc, char const *argv[]){
     int a;
     int b;
     scanf("%d", &a);
     scanf("%d", &b);
     printf("%d",mdc(a,b));
+    return 0;
+}
+*/
+
+int main(int argc, char const *argv[]){
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int r;
+    int mdc;
+    while(a > 0 && b > 0){
+        r = a%b;
+        a = b;
+        b = r;
+        if (b == 0){
+            mdc = a;
+        }else{
+            mdc = b;
+        }
+    }
+    printf("%d",mdc);
     return 0;
 }
