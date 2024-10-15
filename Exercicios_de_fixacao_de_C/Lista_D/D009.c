4181b62441d7ff993e29ba10e00120d9c4691eb3
RaulggSouza <raulggsouza@hotmail.com>
2024-10-15 12:06:25 -0300

    :sparkles: feat: finish exercise D009

---
+++ Exercicios_de_fixacao_de_C/Lista_D/D009.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    int n;
+    scanf("%d", &n);
+    int* list = (int*) malloc(sizeof(int)*n);
+    int num;
+    for (int i = 0; i < n; i++){
+        scanf("%d", &num);
+        list[i] = num;
+    }
+    int i;
+    int j;
+    scanf("%d", &i);
+    scanf("%d", &j);
+    int soma = 0;
+    int a;
+    int b;
+    if (i <= n && j <= n){
+        if (j > i){
+            a = i;
+            b = j;
+        }else{
+            b = i;
+            a = j;
+        }
+        for (int k = a; k <= b; k++){
+            soma += list[k];
+        }
+        printf("%d", soma);
+    }else{
+        printf("INVALIDO");
+    }
+    return 0;
+}
