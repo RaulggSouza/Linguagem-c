d4cdde9b7de8bf7bf2b5b5b159ebf8d7d7ce36c8
RaulggSouza <raulggsouza@hotmail.com>
2024-10-15 08:59:17 -0300

    :sparkles: feat: finish exercise D001

---
+++ Exercicios_de_fixacao_de_C/Lista_D/D001.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    int max = 7;
+    int size = 0;
+    int* temperaturas = (int*) malloc(sizeof(int)*max);
+    int temp;
+    int media = 0;
+    int acima = 0;
+    for (int i = 0; i < max; i++){
+        scanf("%d", &temp);
+        size++;
+        media += temp;
+        temperaturas[i] = temp;
+    }
+    media /= size;
+    for (int i = 0; i < size; i++){
+        if (temperaturas[i] > media){
+            acima++;
+        }
+    }
+    printf("%d", acima);
+    return 0;
+}
