38635f76a639bcc12bcb746ef0f38cfc75f0118c
RaulggSouza <raulggsouza@hotmail.com>
2024-10-15 11:51:33 -0300

    :sparkles: feat: finish exercise D007

---
+++ Exercicios_de_fixacao_de_C/Lista_D/D007.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    int n;
+    scanf("%d", &n);
+    int* list = (int*) malloc(sizeof(int)*n);
+    int num;
+    for (int i = 0; i < n; i++){
+        scanf("%d", &num);
+        list[i] = num;
+    }
+    int aux;
+    for (int i = 0; i+1 < n; i+=2){
+        aux = list[i];
+        list[i] = list[i+1];
+        list[i+1] = aux;
+    }
+    for (int i = 0; i < n; i++){
+        printf("%d ", list[i]);
+    }
+    
+    return 0;
+}
