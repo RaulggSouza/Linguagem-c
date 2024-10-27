71113b05135efa2cdb602abdbfc99ab635a1d84d
RaulggSouza <raulggsouza@hotmail.com>
2024-10-27 17:31:39 -0300

    :sparkles: feat: finish exercise F002

---
+++ Exercicios_de_fixacao_de_C/Lista_F/F002.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    int linhas;
+    scanf("%d", &linhas);
+    int colunas;
+    scanf("%d", &colunas);
+    int** matrix;
+    matrix = (int**) malloc(sizeof(int*) * linhas);
+    for (int i = 0; i < linhas; i++){
+        matrix[i] = (int*) malloc(sizeof(int) * colunas);
+    }
+    
+    for (int i = 0; i < linhas; i++){
+        for (int j = 0; j < colunas; j++){
+            int num;
+            scanf("%d", &num);
+            matrix[i][j] = num;
+        }
+        
+    }
+    
+    for (int i = 0; i < linhas; i++){
+        int soma = 0;
+        for (int j = 0; j < colunas; j++){
+            soma += matrix[i][j];
+        }
+        printf("%d\n", soma);
+    }
+
+    for (int i = 0; i < linhas; i++){
+        free(matrix[i]);
+    }
+    free(matrix);
+    return 0;
+}
