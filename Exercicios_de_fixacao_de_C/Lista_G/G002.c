609c2f52e869f5a45119860af86b092c43b939a0
RaulggSouza <raulggsouza@hotmail.com>
2024-11-04 11:38:18 -0300

    :sparkles: feat: finish exercise G002

--- Exercicios_de_fixacao_de_C/Lista_G/G002.c
+++ Exercicios_de_fixacao_de_C/Lista_G/G002.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printVetor(int* vet, int size){
+    for (int i = 0; i < size; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+}
+
 void populaVetor(int* vet, int* size, int n){
     for (int i = 0; i < n; i++){
         int num;
@@ -10,11 +17,11 @@ void populaVetor(int* vet, int* size, int n){
     }
 }
 
-void remover(int* vet, int* size, int index){
-    for (int i = index; i < size-1; i++){
+void removerElemento(int* vet, int* size, int index){
+    for (int i = index; i < (*size)-1; i++){
         vet[i] = vet[i+1];
     }
-    
+    (*size)--;
 }
 
 int main(int argc, char const *argv[]){
@@ -23,8 +30,14 @@ int main(int argc, char const *argv[]){
     scanf("%d", &n);
     int* vet = (int*) malloc(sizeof(int) * n);
     populaVetor(vet, &size, n);
+    printVetor(vet, size);
     int i;
     scanf("%d", &i);
-
+    if (i < size){
+        removerElemento(vet, &size, i);
+        printVetor(vet, size);
+    }else{
+        printf("INVALIDO");
+    }
     return 0;
 }
