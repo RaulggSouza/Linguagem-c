e9d76b33f31df2f1f53b8d89adfa3a8b6ccda0ec
RaulggSouza <raulggsouza@hotmail.com>
2024-11-04 16:07:35 -0300

    :boom: fix: decrease the len in for by one

--- Exercicios_de_fixacao_de_C/Lista_G/G007.c
+++ Exercicios_de_fixacao_de_C/Lista_G/G007.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[]){
     int len = strlen(str);
     char vowels[5] = {'a','e','i','o','u'};
     int count = 0;
-    for (int i = 0; i < len; i++){
+    for (int i = 0; i < len-1; i++){
         for (int j = 0; j < 5; j++){
             if(str[i] == vowels[j]){
                 count++;
