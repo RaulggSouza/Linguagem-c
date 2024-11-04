76307ee4db32e8fb01280c769c99d0d5d3216d07
RaulggSouza <raulggsouza@hotmail.com>
2024-11-04 19:47:49 -0300

    :sparkles: feat: finish exercise G009

---
+++ Exercicios_de_fixacao_de_C/Lista_G/G009.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]){
+    int n;
+    scanf("%d", &n);
+    int* bin = (int*) malloc(sizeof(int)*64);
+    int i = 0;
+    while (1){
+        bin[i] = n%2;
+        n/=2;
+        i++;
+        if (n == 1 || n == 0){
+            break;
+        }
+    }
+    bin[i] = n;
+    for (int j = i; j >= 0; j--){
+        printf("%d", bin[j]);
+    }
+    return 0;
+}
