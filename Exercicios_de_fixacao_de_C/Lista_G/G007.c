cba47746033e7e78c0300c89ffbb2f594d37ccd2
RaulggSouza <raulggsouza@hotmail.com>
2024-11-04 15:42:55 -0300

    :sparkles: feat: finish exercise G007

---
+++ Exercicios_de_fixacao_de_C/Lista_G/G007.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]){
+    char str[32];
+    fgets(str, 32, stdin);
+    int len = strlen(str);
+    char vowels[5] = {'a','e','i','o','u'};
+    int count = 0;
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < 5; j++){
+            if(str[i] == vowels[j]){
+                count++;
+            }
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
